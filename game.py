bfc0dde0cc366e8221cbc74870a92672ebca4a58
Alfo5123 <alfredo.delafuente.b@gmail.com>
2017-07-08 18:53:01 -0500

    Uplaod game dynamics

--- game.py
+++ game.py
@@ -1,17 +1,24 @@
+#!/usr/bin/env python
+
+# Alfredo de la Fuente 2017
+
 ## GUI Toolkit
 from Tkinter import *
 import tkFont 
 
+## GUI Configuration
+
 class Info(Frame):
+	## Message in the top of screen
     def __init__(self, master=None):
         Frame.__init__(self)
-        self.configure(width=500, height=100)
-        police = tkFont.Font(family="Helvetica",size=36,weight="bold") 
-        self.t = Label(self, text="Connect4 AI", font=police)
+        self.configure(width=500, height=100, bg="white")
+        police = tkFont.Font(family="Arial",size=36,weight="bold") 
+        self.t = Label(self, text="Connect4 AI", font=police, bg ="white")
         self.t.grid(sticky=NSEW, pady=20)
 
 class Point(object):
-
+	## Each one of the circles in the board
     def __init__(self, x, y, canvas, color="white"):
         self.canvas = canvas
         self.x = x
@@ -21,156 +28,215 @@ class Point(object):
         self.r = 30
         self.point = self.canvas.create_oval(self.x+10,self.y+10,self.x+61,self.y+61,fill=color,outline="blue")
 
-    def changeColor(self, color):
+    def setColor(self, color):
         self.canvas.itemconfigure(self.point, fill=color)
         self.color = color
 
+
+class Board(object):
+	## Game basic dynamics
+    def __init__(self, board ):
+    	self.board = board 
+
+    def makeMove(self, move):
+    	# Takes the current board and a possible move specified 
+    	# by the column. Returns the appropiate row where the 
+    	# piece and be located. If it's not found it returns -1.
+
+    	if ( move < 0 or move > 7 or self.board[0][move] != 0 ) return -1 ;
+    	for i in range(len(self.board)):
+    		if ( self.board[i][move] != 0 ):
+    			return i-1
+    	return len(board)-1
+
+    def terminal(self):
+       # Returns true when the game is finished
+        empty = 0 
+        for i in range(len(self.board)):
+       	  for j in range(len(self.board[0])):
+       		if ( self.board[i][j] == 0 ):
+       			empty = empty + 1 
+        return empty==0
+
+    def legal_plays(self):
+        # Takes a sequence of game states representing the full
+        # game history, and returns the full list of moves that
+        # are legal plays for the current player.
+        legal = []
+        for i in range(len(self.board[0])):
+        	if( self.board[0][i] == 0 ):
+        		legal.append(i)
+
+        return legal
+
+    def winner(self):
+        # Takes the board as input and determines if there is a winner.
+        # If the game is now won, return the player number (Computer = 1, Human = -1).
+        # If the game is still ongoing, return zero.  
+	    i = 0
+	    while(i < len(self.board)):
+	        j = 0
+	        while(j < 4):
+	            if(self.board[i][j]== self.board[i][j+1] == self.board[i][j+2] == self.board[i][j+3] == 1):
+	                return 1
+	            elif(self.board[i][j] == self.board[i][j+1] == self.board[i][j+2] == self.board[i][j+3] == -1):
+	                return -1
+	            j +=1
+	        i += 1
+
+
+	    i = 0
+	    while(i < 3):
+	        j = 0
+	        while(j < len(self.board[i])):
+	            if(self.board[i][j] == self.board[i+1][j] == self.board[i+2][j] == self.board[i+3][j] == 1):
+	                return 1
+	            elif(self.board[i][j] == self.board[i+1][j] == self.board[i+2][j] == self.board[i+3][j] == -1):
+	                return -1
+	            j+=1
+	        i+=1
+
+
+	    i = 0
+	    while(i < 3):
+	        j = 0
+	        while(j < 3):
+	            if(self.board[i][j] == self.board[i+1][j+1] == self.board[i+2][j+2] == self.board[i+3][j+3] == 1):
+	                return 1
+	            elif(self.board[i][j] == self.board[i+1][j+1] == self.board[i+2][j+2] == self.board[i+3][j+3] == -1):
+	                return -1
+	            j += 1
+	        i += 1
+	                    
+	    i = 0
+	    while(i < 3):
+	        j = len(self.board[i])-1
+	        while(j > len(self.board)-4):
+	            if(self.board[i][j] == self.board[i+1][j-1] == self.board[i+2][j-2] == self.board[i+3][j-3] == 1):
+	                return 1
+	            elif(self.board[i][j] == self.board[i+1][j-1] == self.board[i+2][j-2] == self.board[i+3][j-3] == -1):
+	                return -1
+	            j -= 1
+	        i += 1
+
+	    return 0
+
+
 class Terrain(Canvas):
+	## Board visual representation
     def __init__(self, master=None):
+
         Canvas.__init__(self)
         self.configure(width=500, height=400, bg="blue")
 
         self.turn = 1
         self.color = "yellow"
         self.p = []
-        self.perm = True
+        self.winner = False
+
+        board = [] 
+        for i in range(6):
+        	row = []
+        	for j in range(7):
+        		row.append(0)
+        	board.append(row)
+
+        self.b = Board( board )
         
         for i in range(0, 340, int(400/6)):
-            liste_rangee = []
+            spots = []
             for j in range(0, 440, int(500/7)):
-                liste_rangee.append(Point(j, i ,self))
+                spots.append(Point(j, i ,self))
                 
-            self.p.append(liste_rangee)
+            self.p.append(spots)
         
-        self.bind("<Button-1>", self.detCol)
+        self.bind("<Button-1>", self.action)
 
-    def detCol(self, event):
-        if self.perm:
+    def action(self, event):
+        if not self.winner:
             col = int(event.x/71)
             lig = 0
-            
+            ok = False 
+
             lig = 0
             while lig < len(self.p):            
-                if self.p[0][col].color == "red" or self.p[0][0].color == "yellow":
+                if self.p[0][col].color == "red" or self.p[0][col].color == "yellow":
                     break
                 
                 if self.p[lig][col].color == "red" or self.p[lig][col].color == "yellow":
-                    self.p[lig-1][col].changeColor(self.color)
+                    self.p[lig-1][col].setColor(self.color)
+                    if ( self.color == "yellow"):
+                    	self.b.board[lig-1][col] = -1
+                    elif ( self.color == "red"):
+                    	self.b.board[lig-1][col] = 1
+                    ok = True
                     break
                 
                 elif lig == len(self.p)-1:
-                    self.p[lig][col].changeColor(self.color)
+                    self.p[lig][col].setColor(self.color)
+                    if ( self.color == "yellow"):
+                    	self.b.board[lig][col] = -1
+                    elif ( self.color == "red"):
+                    	self.b.board[lig][col] = 1
+                    ok = True
                     break
 
-                
                 if self.p[lig][col].color != "red" and self.p[lig][col].color != "yellow":
                     lig+=1
 
             
-            if self.turn == 1:
+            if self.turn == 1 and ok:
                 self.turn = 2
                 info.t.config(text="Computer's Turn")
                 self.color = "red"
 
-            elif self.turn == 2:
+
+            elif self.turn == 2 and ok:
                 self.turn = 1
                 info.t.config(text="Your turn")
                 self.color = "yellow"
 
-            self.Horizontal()
-            self.Vertical()
-            self.Diagonal1()
-            self.Diagonal2()
+            result = self.b.winner()
 
-    def Horizontal(self):
-        i = 0
-        while(i < len(self.p)):
-            j = 0
-            while(j < 4):
-                if(self.p[i][j].color == self.p[i][j+1].color == self.p[i][j+2].color == self.p[i][j+3].color == "red"):
-                    info.t.config(text="Victoire de rouge !")
-                    self.perm = False
-                    break
-                elif(self.p[i][j].color == self.p[i][j+1].color == self.p[i][j+2].color == self.p[i][j+3].color == "yellow"):
-                    info.t.config(text="Victoire de Jaune !")
-                    self.perm = False
-                    break
-                j +=1
-            i += 1
-
-    def Vertical(self):
-        i = 0
-        while(i < 3):
-            j = 0
-            while(j < len(self.p[i])):
-                if(self.p[i][j].color == self.p[i+1][j].color == self.p[i+2][j].color == self.p[i+3][j].color == "red"):
-                    info.t.config(text="Victoire de rouge !")
-                    self.perm = False
-                    break
-                elif(self.p[i][j].color == self.p[i+1][j].color == self.p[i+2][j].color == self.p[i+3][j].color == "yellow"):
-                    info.t.config(text="Victoire de Jaune !")
-                    self.perm = False
-                    break
-                j+=1
-            i+=1
-
-    def Diagonal1(self):
-        i = 0
-        while(i < 3):
-            j = 0
-            while(j < 3):
-                if(self.p[i][j].color == self.p[i+1][j+1].color == self.p[i+2][j+2].color == self.p[i+3][j+3].color == "red"):
-                    info.t.config(text="Victoire de rouge !")
-                    self.perm = False
-                    break
-                elif(self.p[i][j].color == self.p[i+1][j+1].color == self.p[i+2][j+2].color == self.p[i+3][j+3].color == "yellow"):
-                    info.t.config(text="Victoire de Jaune !")
-                    self.perm = False
-                    break
-                j += 1
-            i += 1
-                    
-    def Diagonal2(self):
-        i = 0
-        while(i < 3):
-            j = len(self.p[i])-1
-            while(j > len(self.p)-4):
-                if(self.p[i][j].color == self.p[i+1][j-1].color == self.p[i+2][j-2].color == self.p[i+3][j-3].color == "red"):
-                    info.t.config(text="Victoire de rouge !")
-                    self.perm = False
-                    break
-                elif(self.p[i][j].color == self.p[i+1][j-1].color == self.p[i+2][j-2].color == self.p[i+3][j-3].color == "yellow"):
-                    info.t.config(text="Victoire de Jaune !")
-                    self.perm = False
-                    break
-                j -= 1
-            i += 1
+            #Check if there is a winner or if it ended in a draw
+            if result == 1:
+            	info.t.config(text="You lost!")
+            	self.winner = True 
+            elif result == -1:
+            	info.t.config(text="You won!")
+            	self.winner = True
+            elif self.b.terminal():
+            	info.t.config(text="Draw")
+            	self.winner = True 
+           
 
 
+if __name__ == "__main__":
+	## Game execution
+	root = Tk()
+	root.geometry("500x550")
+	root.title("Connect 4 AI Bot")
+	root.configure(bg="white")
+	root.minsize(500,550)
+	root.maxsize(500,550)
 
-root = Tk()
-root.geometry("500x550")
-root.title("Connect 4 AI Bot")
-root.minsize(500,550)
-root.maxsize(500,550)
-
-info = Info(root)
-info.grid(row=0, column=0)
+	info = Info(root)
+	info.grid(row=0, column=0)
 
 
-t = Terrain(root)
-t.grid(row=1, column=0)
+	t = Terrain(root)
+	t.grid(row=1, column=0)
 
-def rein():
-    global info
-    info.t.config(text="")
-    
-    info = Info(root)
-    info.grid(row=0, column=0)
+	def restart():
+	    global info
+	    info.t.config(text="")
+	    
+	    info = Info(root)
+	    info.grid(row=0, column=0)
 
-    t = Terrain(root)
-    t.grid(row=1, column=0)
+	    t = Terrain(root)
+	    t.grid(row=1, column=0)
 
-Button(root, text="Try again (?)", command=rein).grid(row=2, column=0, pady=30)
+	Button(root, text="Try again (?)", command=restart).grid(row=2, column=0, pady=15)
 
-root.mainloop()
+	root.mainloop()
